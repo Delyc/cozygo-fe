74d976fd672c0989fe79107c64f4874f13dc500a
delyc <delyce35@gmail.com>
2024-01-22 13:42:54 +0200

    feat: card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,24 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto grid grid-cols-3 gap-5 px-20 py-20 ">
     <PropertyCard
+      bedrooms={3}
+      baths={3}
+      area={340}
+      price={25000.00}
+      address="Meadowview Lane, Tranquil Springs"
+    />
+
+<PropertyCard
+      bedrooms={3}
+      baths={3}
+      area={340}
+      price={25000.00}
+      address="Meadowview Lane, Tranquil Springs"
+    />
+
+<PropertyCard
       bedrooms={3}
       baths={3}
       area={340}
--- src/components/cards/House.tsx
+++ src/components/cards/House.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Heart from '../svgs/Heart';
+import { ArrowIcon, HeartIcon, LocationIcon, RoomIcon } from '../svgs/Heart';
 
 type PropertyCardProps = {
   bedrooms: number;
@@ -17,24 +17,47 @@ const PropertyCard: React.FC<PropertyCardProps> = ({
   address,
 }) => {
   return (
-    <div className="max-w-sm    flex flex-col items-center justify-center relative">
-      <img className="w-full h-60 rounded-3xl" src="./assets/house.jpeg" alt="House" />
-      <div className='absolute top-48 bg-white rounded-3xl shadow-2xl'>
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">
+    <div className="max-w-[30rem]  flex flex-col items-center justify-center relative">
+      <img className="w-full h-[200px] rounded-3xl" src="./assets/house.jpeg" alt="House" />
+      <div className='absolute top-36 left-5 right-5 bg-white py-4 rounded-3xl shadow-2xl'>
+      <div className="px-5 flex flex-col gap-3">
+        <div className="font-bold text-xl flex items-center justify-between">
           <p>${price.toFixed(2)}</p>
-          <div>
-            <div className='bg-indigo-600 w-4 h-4 rounded-full'>
-              <Heart fill={"#4f46e5"} height={"20px"} width={"20px"} />
+          <div className='flex gap-2.5'>
+            <div className='bg-indigo-600/20 grid place-content-center w-8 h-8 rounded-full'>
+              <HeartIcon fill={"#4f46e5"} height={"18px"} width={"18px"} stroke={"#4f46e5"} stroke_width={2}/>
+            </div>
+            <div className='bg-indigo-600 grid place-content-center w-8 h-8 rounded-full'>
+              <ArrowIcon fill={"#fff"} height={"20px"} width={"18px"} stroke={"#fff"} stroke_width={1}/>
             </div>
           </div>
           </div>
-        <p className="text-gray-700 text-base">{address}</p>
+          <div className='flex flex-col gap-1 border-gray-200 pb-5 border-b'>
+
+        <p className="text-primary_gray font-jost  text-base">{address}</p>
+        <p className="text-primary_gray  text-xs font-normal">{address}</p>
+
+          </div>
       </div>
-      <div className="px-6 pt-4 pb-2">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{bedrooms} Bed</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{baths} Bath</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">{area} sq.ft</span>
+      <div className="px-5 flex items-center gap-2 py-2">
+        <div className='flex items-center  gap-1'>
+          <RoomIcon fill={'#757B8D'} height={'20px'} width={'20px'} stroke={'#757B8D'} stroke_width={0} />
+          <p className=' text-primary_gray text-xs'>{bedrooms} rooms</p>
+        </div>
+
+        <div className='flex items-center  gap-1'>
+          <RoomIcon fill={'#757B8D'} height={'20px'} width={'20px'} stroke={'#757B8D'} stroke_width={0} />
+          <p className=' text-primary_gray text-xs'>rooms</p>
+        </div>
+
+        <div className='flex items-center  gap-1'>
+          <LocationIcon fill={'#757B8D'} height={'20px'} width={'20px'} stroke={'#757B8D'} stroke_width={0} />
+          <p className=' text-primary_gray text-xs'>Location</p>
+        </div>
+      </div>
+
+      <div className='flex px-5'>
+        <button className='bg-indigo-600 text-white text-xs rounded w-full px-6 py-3'>Book A Visit</button>
       </div>
       </div>
     </div>
