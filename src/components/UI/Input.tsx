3f32f2cbd1ec8aba5f0488606cf0799a67a2a25b
delyc <delyce35@gmail.com>
2024-01-26 11:22:12 +0200

    feat: adding input validation

--- src/components/UI/Input.tsx
+++ src/components/UI/Input.tsx
@@ -1,5 +1,6 @@
 'use client'
 import React, { useState } from 'react';
+import { z, ZodType } from 'zod';
 
 interface FloatingLabelInputProps {
   id: string;
@@ -8,7 +9,11 @@ interface FloatingLabelInputProps {
   value?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   className?: string;
+  validateOnChange?: boolean;
+  schema?: ZodType<any>;
 }
+
+
 
 const FloatingLabelInput: React.FC<FloatingLabelInputProps> = ({
   id,
@@ -16,38 +21,69 @@ const FloatingLabelInput: React.FC<FloatingLabelInputProps> = ({
   type = 'text',
   value,
   onChange,
-  className
+  className,
+  schema,
+  validateOnChange = false,
 }) => {
   const [isFocused, setIsFocused] = useState(false);
   const [inputValue, setInputValue] = useState(value || '');
+  const [error, setError] = useState<string | null>(null);
 
   const handleFocus = () => setIsFocused(true);
-  const handleBlur = () => setIsFocused(inputValue.length > 0);
+  const handleBlur = () => {
+    setIsFocused(inputValue.length > 0);
+    validateInput(inputValue);
+  };
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.target.value);
+    const newValue = e.target.value;
+    setInputValue(newValue);
+    if (validateOnChange) {
+      validateInput(newValue);
+    }
     if (onChange) onChange(e);
   };
 
+
+  const validateInput = (value: string) => {
+    if (schema) {
+      try {
+        schema.parse(value);
+        setError(null);
+      } catch (error) {
+        if (error instanceof z.ZodError) {
+          setError(error.errors[0].message);
+        }
+      }
+    }
+  };
+
+
+
+
+
   return (
-    <div className={`relative border-2 rounded-md shadow-sm focus-within:border-indigo-600 ${className}`}>
-      <input
-        id={id}
-        type={type}
-        value={inputValue}
-        onChange={handleChange}
-        onFocus={handleFocus}
-        onBlur={handleBlur}
-        className="block w-full pt-3  pb-3 px-3 focus:outline-none text-sm text-gray-700 bg-transparent"
-      />
-      <label
-        htmlFor={id}
-        className={`absolute left-3 top-3 px-1 text-gray-500 text-sm transition-all ${
-          isFocused || inputValue ? 'transform -translate-y-6 scale-75 bg-white' : 'pointer-events-none'
-        }`}
-      >
-        {label}
-      </label>
-    </div>
+    <>
+      <div className={`relative border-2 rounded-md shadow-sm focus-within:border-indigo-600 ${className}`}>
+        <input
+          id={id}
+          type={type}
+          value={inputValue}
+          onChange={handleChange}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          className="block w-full pt-3  pb-3 px-3 focus:outline-none text-sm text-gray-700 bg-transparent"
+        />
+        <label
+          htmlFor={id}
+          className={`absolute left-3 top-3 px-1 text-gray-500 text-sm transition-all ${isFocused || inputValue ? 'transform -translate-y-6 scale-75 bg-white' : 'pointer-events-none'
+            }`}
+        >
+          {label}
+        </label>
+      </div>
+      {error && <p className="text-red-500 text-xs italic">{error}</p>}
+    </>
   );
 };
 
--- src/components/forms/HouseForm.tsx
+++ src/components/forms/HouseForm.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import FloatingLabelInput from '../UI/Input';
-
+import { z, ZodType } from 'zod';
 
 const HouseForm = () => {
 
+    const emailSchema = z.string().email({ message: "Invalid email address" });
+
+// Schema for a numeric input
+const numberSchema = z.string().regex(/^\d+$/, { message: "Only numbers are allowed" });
+
+// Schema for a password input
+const passwordSchema = z.string().min(8, { message: "Password must be at least 8 characters" });
+
     return (
         <form className='flex flex-col gap-3'>
-            <FloatingLabelInput id={''} label={'House location'} />
-            <FloatingLabelInput id={''} label={'House location'} />
+            <FloatingLabelInput id={''} label={'House location'}   schema={emailSchema}
+type='email'/>
+            <FloatingLabelInput id={''} label={'phone number'}   schema={numberSchema}
+type='text'/>
             <FloatingLabelInput id={''} label={'House location'} />
             <FloatingLabelInput id={''} label={'House location'} />
             <FloatingLabelInput id={''} label={'House location'} />
