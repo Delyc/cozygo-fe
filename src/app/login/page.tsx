21b44627b350f44e7a581c97d8792ecb23beed0f
delyc <delyce35@gmail.com>
2024-03-03 16:32:05 +0200

    feat: adding loading spinner

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,6 +9,8 @@ import FloatingLabelInput from '@/components/UI/Input';
 import Button from '@/components/UI/Button';
 import Footer from '@/components/layout/Footer';
 import { useRouter } from 'next/navigation';
+import { ToastContainer, toast } from 'react-toastify';
+import { decodeToken } from '@/helpers/decodeToken';
 function Login() {
   const router = useRouter()
   const [login, { isLoading, data, error }] = useLoginMutation();
@@ -28,17 +30,35 @@ function Login() {
 
     try {
       const user = await login(credentials).unwrap();
-      console.log(user, "userrrr")
       localStorage.setItem("token", user.token)
-      // Handle success, e.g., navigate to the dashboard or show a success message
-    } catch (error) {
-      // Handle login error
+      toast.success("Successfully Logged in")
+      if(decodeToken(user.token)?.accountType === "homeSeeker"){
+        router.push("/houses")
+
+      }else{
+      router.push("/agent")
+
+      }
+    } catch (error: any) {
+      console.log(error, "error")
+      toast("Wrong credentials")
     }
   };
 
   return (
 
     <section className="flex flex-col bg-white items-center ">
+
+<button type="button" className="bg-indigo-600 text-white ..." disabled>
+  
+
+  <svg className="animate-spin h-5 w-5 mr-3 ..." width="30px" height="30px" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+  <path fill="white" fill-rule="evenodd" d="M11,16 C12.1045695,16 13,16.8954305 13,18 C13,19.1045695 12.1045695,20 11,20 C9.8954305,20 9,19.1045695 9,18 C9,16.8954305 9.8954305,16 11,16 Z M4.74123945,13 C6.12195133,13 7.24123945,14.1192881 7.24123945,15.5 C7.24123945,16.8807119 6.12195133,18 4.74123945,18 C3.36052758,18 2.24123945,16.8807119 2.24123945,15.5 C2.24123945,14.1192881 3.36052758,13 4.74123945,13 Z M16.3193286,13.5 C17.4238981,13.5 18.3193286,14.3954305 18.3193286,15.5 C18.3193286,16.6045695 17.4238981,17.5 16.3193286,17.5 C15.2147591,17.5 14.3193286,16.6045695 14.3193286,15.5 C14.3193286,14.3954305 15.2147591,13.5 16.3193286,13.5 Z M18.5,9.31854099 C19.3284271,9.31854099 20,9.99011387 20,10.818541 C20,11.6469681 19.3284271,12.318541 18.5,12.318541 C17.6715729,12.318541 17,11.6469681 17,10.818541 C17,9.99011387 17.6715729,9.31854099 18.5,9.31854099 Z M2.5,6 C3.88071187,6 5,7.11928813 5,8.5 C5,9.88071187 3.88071187,11 2.5,11 C1.11928813,11 0,9.88071187 0,8.5 C0,7.11928813 1.11928813,6 2.5,6 Z M17.7857894,5.20724734 C18.3380741,5.20724734 18.7857894,5.65496259 18.7857894,6.20724734 C18.7857894,6.75953209 18.3380741,7.20724734 17.7857894,7.20724734 C17.2335046,7.20724734 16.7857894,6.75953209 16.7857894,6.20724734 C16.7857894,5.65496259 17.2335046,5.20724734 17.7857894,5.20724734 Z M8,0 C9.65685425,0 11,1.34314575 11,3 C11,4.65685425 9.65685425,6 8,6 C6.34314575,6 5,4.65685425 5,3 C5,1.34314575 6.34314575,0 8,0 Z M15.5,3 C15.7761424,3 16,3.22385763 16,3.5 C16,3.77614237 15.7761424,4 15.5,4 C15.2238576,4 15,3.77614237 15,3.5 C15,3.22385763 15.2238576,3 15.5,3 Z"/>
+</svg>
+  Processing...
+</button>
+       <ToastContainer />
+
       <div className="flex px-20 py-20 bg-white justify-center gap-20 items-center w-4/6 ">
         <div className="relative w-1/2 pt-20 pb-20 pl-20 bg-indigo-600 rounded-3xl">
           <div className="flex flex-col gap-2.5 w-3/4  ml-16">
@@ -128,7 +148,7 @@ function Login() {
             onChange={handleChange}
             label="Password"
             required id='password' />
-          <Button type="submit" disabled={isLoading} label="Login" className='text-white' />
+          <Button type="submit" disabled={isLoading} label={isLoading ? 'Loading ....': 'Login'} className='text-white' />
         <p className='text-primary_gray'>Don{"'"}t have an account? <span className='font-bold text-indigo-600' onClick={() => router.push("/register")}>Register</span> here</p>
 
         </form>
